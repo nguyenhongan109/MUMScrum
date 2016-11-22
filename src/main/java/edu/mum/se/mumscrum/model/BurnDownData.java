4d7b3c1a8ed62cde8a442e2e5dd6ca3d29c2c18e
mehdi <bayar.mehdi@gmail.com>
2016-11-22 05:32:30 -0600

    BurnDownChart

--- src/main/java/edu/mum/se/mumscrum/model/BurnDownData.java
+++ src/main/java/edu/mum/se/mumscrum/model/BurnDownData.java
@@ -1,7 +1,47 @@
 package edu.mum.se.mumscrum.model;
 
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.sql.Date;
+
 /**
  * Created by mehdi on 11/20/16.
  */
+
+
 public class BurnDownData {
+
+
+
+    private Date updatedDate;
+    private Double duration;
+
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+    public Date getUpdatedDate() {
+
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "BurnDownData{" +
+                "updatedDate=" + updatedDate +
+                ", duration=" + duration +
+                '}';
+    }
 }
