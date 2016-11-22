1bd85ca7b76bb171818d62bebdd6a38868b15d1c
mehdi <bayar.mehdi@gmail.com>
2016-11-22 05:33:22 -0600

    BurnDownChart

--- src/main/java/edu/mum/se/mumscrum/service/BurnDownChartService.java
+++ src/main/java/edu/mum/se/mumscrum/service/BurnDownChartService.java
@@ -1,9 +1,18 @@
 package edu.mum.se.mumscrum.service;
 
+import edu.mum.se.mumscrum.model.BurnDownChartData;
+import edu.mum.se.mumscrum.model.BurnDownData;
+import edu.mum.se.mumscrum.model.Timelogs;
+
+import java.util.List;
+
 /**
  * Created by mehdi on 11/17/16.
  */
-public interface BurnDownChart {
+public interface BurnDownChartService {
 
+
+    public  BurnDownChartData getBurnDownData(int sid);
+    //public BurnDownChartData getBurnDownChartData();
 
 }
